040e3a5df31c1e8b5faf99d65449ff551ca915d2
gift <giftngobeni@thedigitalacademy.co.za>
2019-07-04 08:35:11 +0200

    api key edit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: true,
 	apiKey: '1cbf8023de2849e4848191b88babaf44',
 	apiUrl: 'https://newsapi.org/v2',
-	apiDefaultCountry: 'us'
+	apiDefaultCountry: 'sa'
 };
